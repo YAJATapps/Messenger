a6a056f1f1eacaa58fdbdd101277f98449d1b336
Yajat <yajatapps3@gmail.com>
2021-04-01 13:30:06 -0700

    Show no message selected initially when no message profile is selected

--- src/components/MessagesView.jsx
+++ src/components/MessagesView.jsx
@@ -3,6 +3,14 @@ import './MessagesView.css';
 
 export default class MessagesView extends Component {
     render() {
+        if (this.props.selected === '') {
+            return (
+                <div className='messagesView'>
+                    <h1 className='centreError'>No message profile selected. Select from the messsages view or send someone a new message.</h1>
+                </div>
+            )
+        }
+
         return (
             <div className='messagesView'>
                 <h1>{this.props.selected}</h1>
