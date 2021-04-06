3f3fa53cc993f3c857cc72cc2498f1c00eed6971
Yajat <yajatapps3@gmail.com>
2021-04-05 19:15:44 -0700

    Fetch the profiles from API

--- src/components/MessageMenu.jsx
+++ src/components/MessageMenu.jsx
@@ -5,13 +5,44 @@ import MessageProfile from './MessageProfile';
 // The list of persons to message
 export default class MessageMenu extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { profiles: '' };
+    }
+
+    // Load profiles after the component mounts
+    componentDidMount() {
+        this.loadProfiles();
+    }
+
     render() {
         return (
             <div className='messageMenu'>
-                <MessageProfile name='Person1' id='0' clickAt={this.props.clickAt}/>
-                <MessageProfile name='Person2' id='1' clickAt={this.props.clickAt}/>
-                <MessageProfile name='Person3' id='2' clickAt={this.props.clickAt}/>
+                {Array.isArray(this.state.profiles) && this.state.profiles.map((value, index) => {
+                    return <MessageProfile key={index} name={value.name} id={value.id} clickAt={this.props.clickAt} />
+                })}
             </div>
         )
+    }
+
+    // Fetch the profiles from API and load it into profiles state
+    loadProfiles() {
+        let url = 'http://127.0.0.1:5000/api/v1/messenger/profiles';
+        fetch(url)
+            .then(res => this.checkError(res))
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                    profiles: result
+                });
+            })
+            .catch(error => console.log(error));
+    }
+
+    checkError(response) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response;
     }
 }
